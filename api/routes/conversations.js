15abbb32aa39180217898a63746286147038c3d9
devNjan <debanjandebnath05@gmail.com>
2025-11-07 13:39:25 +0530

    Created conversations.js to handle user conversation routes

---
+++ api/routes/conversations.js
@@ -0,0 +1,41 @@
+const Conversation = require("../models/Conversation");
+
+//New conversation
+router.post("/", async (req, res) => {
+  const newConversation = new Conversation({
+    members: [req.body.senderId, req.body.receiverId],
+  });
+
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(200).json(savedConversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get conversation of a user
+router.get("/:userId", async (req, res) => {
+  try {
+    const converstation = await Conversation.findOne({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get conversation that includes two userId
+router.get("find/:firstUserId/:secondUserId", async (req, res) => {
+  try {
+    const converstation = await Conversation.findOne({
+      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
