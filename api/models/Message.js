8d0bcfdcf7fadcb453db8840404184b184e6ac9b
devNjan <debanjandebnath05@gmail.com>
2025-11-07 14:19:40 +0530

    Corrected mongoose import

--- api/models/Message.js
+++ api/models/Message.js
@@ -1,4 +1,4 @@
-const mongoose = require("Mongoose");
+const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.schema(
   {
--- api/models/Post.js
+++ api/models/Post.js
@@ -1,4 +1,4 @@
-const mongoose = require("Mongoose");
+const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.schema(
   {
