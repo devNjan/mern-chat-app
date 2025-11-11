ca9ce988b9617ce2bb8b14a2188367c1af428be3
devNjan <debanjandebnath05@gmail.com>
2025-11-11 22:55:59 +0530

    Updated Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,6 +3,31 @@ import { useRef } from "react";
 import { useHistory } from "react-router";
 
 function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const history = useHistory();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (passwordAgain.current.value != password.current.value) {
+      passwordAgain.current.setcustomValidity("Passwords do not match!");
+    } else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      };
+      try {
+        await axios.post("auth/register", user);
+        history.push("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -13,23 +38,32 @@ function Register() {
           </span>
         </div>
         <div className="loginRight">
-          <form>
-            <input placeholder="Username" required className="loginInput" />
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Username"
+              required
+              ref={username}
+              className="loginInput"
+            />
             <input
               placeholder="Email"
               required
+              ref={email}
               className="loginInput"
               type="email"
             />
             <input
               placeholder="Password"
               required
+              ref={password}
               className="loginInput"
               type="password"
+              minLength="6"
             />
             <input
               placeholder="Enter Password Again"
               required
+              ref={passwordAgain}
               className="loginInput"
               type="password"
             />
