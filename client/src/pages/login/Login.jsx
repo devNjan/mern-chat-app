a2c1d47bbf2daad8eb19f847830a0a8c5f067621
devNjan <debanjandebnath05@gmail.com>
2025-11-13 22:36:07 +0530

    Updated Login.jsx

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Chat App</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Chat App.
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox"></form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
