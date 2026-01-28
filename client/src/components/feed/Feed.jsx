566642cfae2a2b4fd6a8375c298ddfbccbbbf082
devNjan <debanjandebnath05@gmail.com>
2026-01-28 21:24:50 +0530

    Added styling for conversations component

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
+import "./feed.css";
 import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
 
